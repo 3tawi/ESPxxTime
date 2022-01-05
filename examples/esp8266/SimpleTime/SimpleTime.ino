e9cf7dd3be84d5135d6dbedbd1332dd9a43ba335
3tawi <96850409+3tawi@users.noreply.github.com>
2022-01-05 21:07:08 +0100

    Update SimpleTime.ino

--- examples/esp8266/SimpleTime/SimpleTime.ino
+++ examples/esp8266/SimpleTime/SimpleTime.ino
@@ -16,30 +16,30 @@ void loop() {
   Serial.println(rtc.getTime("%A, %B %d %Y %H:%M:%S"));   // (String) returns time with specified format 
 // formating options  http://www.cplusplus.com/reference/ctime/strftime/
   
-      Serial.println("");          
-      Serial.println(rtc.getTime());          //  (String) 19:58:30
-      Serial.println(rtc.getDate());          //  (String) Thu, Jan 13 2022
-      Serial.println(rtc.getDate(true));      //  (String) Thursday, January 13 2022
-      Serial.println(rtc.getDateTime());      //  (String) Thu, Jan 13 2022 19:58:30
-      Serial.println(rtc.getDateTime(true));  //  (String) Thursday, January 13 2022 19:58:30
-      Serial.println(rtc.getTimeDate());      //  (String) 19:58:30 Thu, Jan 13 2022
-      Serial.println(rtc.getTimeDate(true));  //  (String) 19:58:30 Thursday, January 13 2022
-      Serial.println(""); 
-      Serial.println(rtc.getMicros());        //  (long)    9309
-      Serial.println(rtc.getMillis());        //  (long)    9
-      Serial.println(rtc.getEpoch());         //  (long)    1642103910
-      Serial.println(rtc.getSecond());        //  (int)     30    (0-59)
-      Serial.println(rtc.getMinute());        //  (int)     58    (0-59)
-      Serial.println(rtc.getHour());          //  (int)     7     (0-12)
-      Serial.println(rtc.getHour(true));      //  (int)     19    (0-23)
-      Serial.println(rtc.getDay());           //  (int)     13    (1-31)
-      Serial.println(rtc.getMonth());         //  (int)     0     (0-11)
-      Serial.println(rtc.getYear());          //  (int)     2022
-      Serial.println(rtc.getDayofWeek());     //  (int)     4     (0-6)
-      Serial.println(rtc.getDayofYear());     //  (int)     12    (0-365)
-      Serial.println(rtc.getAmPm());          //  (String)  pm
-      Serial.println(rtc.getAmPm(true));      //  (String)  PM
-      Serial.println(""); 
+      // Serial.println("");          
+      // Serial.println(rtc.getTime());          //  (String) 19:58:30
+      // Serial.println(rtc.getDate());          //  (String) Thu, Jan 13 2022
+      // Serial.println(rtc.getDate(true));      //  (String) Thursday, January 13 2022
+      // Serial.println(rtc.getDateTime());      //  (String) Thu, Jan 13 2022 19:58:30
+      // Serial.println(rtc.getDateTime(true));  //  (String) Thursday, January 13 2022 19:58:30
+      // Serial.println(rtc.getTimeDate());      //  (String) 19:58:30 Thu, Jan 13 2022
+      // Serial.println(rtc.getTimeDate(true));  //  (String) 19:58:30 Thursday, January 13 2022
+      // Serial.println(""); 
+      // Serial.println(rtc.getMicros());        //  (long)    9309
+      // Serial.println(rtc.getMillis());        //  (long)    9
+      // Serial.println(rtc.getEpoch());         //  (long)    1642103910
+      // Serial.println(rtc.getSecond());        //  (int)     30    (0-59)
+      // Serial.println(rtc.getMinute());        //  (int)     58    (0-59)
+      // Serial.println(rtc.getHour());          //  (int)     7     (0-12)
+      // Serial.println(rtc.getHour(true));      //  (int)     19    (0-23)
+      // Serial.println(rtc.getDay());           //  (int)     13    (1-31)
+      // Serial.println(rtc.getMonth());         //  (int)     0     (0-11)
+      // Serial.println(rtc.getYear());          //  (int)     2022
+      // Serial.println(rtc.getDayofWeek());     //  (int)     4     (0-6)
+      // Serial.println(rtc.getDayofYear());     //  (int)     12    (0-365)
+      // Serial.println(rtc.getAmPm());          //  (String)  pm
+      // Serial.println(rtc.getAmPm(true));      //  (String)  PM
+      // Serial.println(""); 
   
   delay(1000);
 }
